37503ed5385f411e36f1ad8cfd0867f0a9fd3f6c
Aimee <aimeejillson@gmail.com>
2018-07-12 18:05:00 -0500

    Add author citation to sw files, fixed address problem for files cached

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-var staticCacheName = 'restaurantProject-v01';
+/*
+* Code adapted from
+* Udacity's course Offline Web Applications by Google,
+* Michael Wales and Jake Archibald instructors:
+* https://www.udacity.com/course/offline-web-applications--ud899
+* and
+* MSDN service worker example on 6/25/2018:
+* https://developers.google.com/web/fundamentals/primers/service-workers/
+*/
+
+var currentCacheName = 'restaurantProject-v01';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
+    caches.open(currentCacheName).then(function (cache) {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/restaurant.html',
-        '/css/styles.css',
-        '/data/restaurants.json',
-        '/js/',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/register_sw.js',
-        '/js/restaurant_info.js'
+        'index.html',
+        'restaurant.html',
+        'css/styles.css',
+        'data/restaurants.json',
+        'js/',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/register_sw.js',
+        'js/restaurant_info.js'
       ]).catch(function (error) {
         console.log('Caches open failed: ' + error);
       });
@@ -27,7 +37,7 @@ self.addEventListener('activate', function (event) {
       return Promise.all(
         cacheNames.filter(function (cacheName) {
           return cacheName.startsWith('restaurantProject-') &&
-            cacheName != staticCacheName;
+            cacheName != currentCacheName;
         }).map(function (cacheName) {
           return caches.delete(cacheName);
         })
@@ -38,30 +48,29 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function (response) {
-      if (response) {
-        return response;
-      }
-
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(
-        function (response) {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          var responseToCache = response.clone();
-
-          caches.open(staticCacheName)
-            .then(function (cache) {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        }
-      );
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
     })
   );
 });
+//      var fetchRequest = event.request.clone();
+//
+//      return fetch(fetchRequest).then(
+//        function (response) {
+//          if (!response || response.status !== 200 || response.type !== 'basic') {
+//            return response;
+//          }
+//
+//          var responseToCache = response.clone();
+//
+//          caches.open(currentCacheName)
+//            .then(function (cache) {
+//              cache.put(event.request, responseToCache);
+//            });
+//
+//          return response;
+//        }
+//      );
+//})
+//);
+//});
