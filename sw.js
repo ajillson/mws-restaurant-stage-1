02a58840c7f6f7ae14c2daaa3d03aedc1821638a
Aimee <aimeejillson@gmail.com>
2018-07-13 13:34:19 -0500

    Update code references, Attempt to fix problem with sw not returning cached images

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
 /*
-* Code adapted from
-* Udacity's course Offline Web Applications by Google,
-* Michael Wales and Jake Archibald instructors:
-* https://www.udacity.com/course/offline-web-applications--ud899
-* and
-* MSDN service worker example on 6/25/2018:
-* https://developers.google.com/web/fundamentals/primers/service-workers/
-*/
+ * Code adapted from
+ * Udacity's course Offline Web Applications by Google,
+ * Michael Wales and Jake Archibald instructors:
+ * https://www.udacity.com/course/offline-web-applications--ud899
+ * and
+ * pulled from Google's Web Fundamentals, Service Workers: an Introduction,
+ * by Matt Gaunt
+ * on 6/25/2018:
+ * https://developers.google.com/web/fundamentals/primers/service-workers/
+ */
 
-var currentCacheName = 'restaurantProject-v01';
+var currentCacheName = 'restaurantProject-v03';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
@@ -48,21 +50,34 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+    caches.match(event.request)
+    .then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
 
+      // Code from https://developers.google.com/web/fundamentals/primers/service-workers/
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
       var fetchRequest = event.request.clone();
 
       return fetch(fetchRequest).then(
         function (response) {
+          // Check if we received a valid response
           if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
 
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
           var responseToCache = response.clone();
 
-          caches.open(staticCacheName)
+          caches.open(currentCacheName)
             .then(function (cache) {
               cache.put(event.request, responseToCache);
             });
