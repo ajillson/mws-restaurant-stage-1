3e2f052349ad161c5aa58be01f8956c169de8341
Aimee <aimeejillson@gmail.com>
2018-07-12 20:22:06 -0500

    Add clone section from MDN back into service worker

--- sw.js
+++ sw.js
@@ -50,27 +50,26 @@ self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
+      }
+
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function (response) {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(staticCacheName)
+            .then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        }
+      );
     })
   );
 });
-//      var fetchRequest = event.request.clone();
-//
-//      return fetch(fetchRequest).then(
-//        function (response) {
-//          if (!response || response.status !== 200 || response.type !== 'basic') {
-//            return response;
-//          }
-//
-//          var responseToCache = response.clone();
-//
-//          caches.open(currentCacheName)
-//            .then(function (cache) {
-//              cache.put(event.request, responseToCache);
-//            });
-//
-//          return response;
-//        }
-//      );
-//})
-//);
-//});
