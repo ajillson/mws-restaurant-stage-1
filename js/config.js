4bc2a0dae66f1c6f49dc83296c8b1378fb1a97a7
ajillson <aimeejillson@gmail.com>
2018-07-15 12:03:44 -0500

    feat: Recreate config.js as requested by Reviewer

---
+++ js/config.js
@@ -0,0 +1,11 @@
+/* File for sensitive information, SEE README.md
+*
+* from derzorngottes/Hide API Keys
+* https://gist.github.com/derzorngottes/3b57edc1f996dddcab25
+* on 07/14/18
+* 
+*/
+
+var config = {
+  MY_MAPBOX_TOKEN : 'PLACE_YOUR_MAPBOX_LEAFLET_TOKEN_HERE';
+}
